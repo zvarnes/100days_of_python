fbae66c8a2a3bb02445f0db33b2815aa76fb89fb
zvarnes <zach.varnes@gmail.com>
2025-03-26 16:15:24 -0400

    day1 complete

---
+++ day1/debugging_practice.py
@@ -0,0 +1,14 @@
+"""
+Debugging Practice
+Look at the code in the code editor. There are errors on all 5 lines of code. Fix the code so that it runs without errors.
+
+Try to run the code and debug each line using the error messages and feedback.
+"""
+
+# Fix the code below 👇
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \\ and the letter n")
